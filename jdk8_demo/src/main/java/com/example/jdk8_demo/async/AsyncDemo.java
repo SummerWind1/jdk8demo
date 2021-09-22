ce404faf2cc63d3776ee632d27ae1eb30ee214ed
panfang <panfang@kugou.net>
2021-09-22 19:40:49 +0800

    [Fix] 线程池

--- jdk8_demo/src/main/java/com/example/jdk8_demo/async/AsyncDemo.java
+++ jdk8_demo/src/main/java/com/example/jdk8_demo/async/AsyncDemo.java
@@ -9,9 +9,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 /**
  * 异步注解使用
@@ -20,12 +18,14 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Configuration
 public class AsyncDemo {
 
+    private static ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 6, TimeUnit.SECONDS, new LinkedBlockingQueue<>(1024));
+
     @Autowired
     private BusinessServiceDemo businessServiceDemo;
 
     @Async("taskExecutor1")
-    public CompletableFuture<List<String>> syncMethodDemo1() throws InterruptedException {
-        Thread.sleep(5000);
+    public CompletableFuture<List<String>> asyncMethodDemo1() throws InterruptedException {
+//        Thread.sleep(5000);
         System.out.println("异步方法执行,当前线程名称：" + Thread.currentThread().getName());
 
         // 业务方法
@@ -38,14 +38,29 @@ public class AsyncDemo {
     }
 
     @Async("taskExecutor2")
-    public CompletableFuture<List<Person>> syncMethodDemo2() throws InterruptedException {
-        Thread.sleep(5000);
+    public CompletableFuture<List<Person>> asyncMethodDemo2() throws InterruptedException {
+//        Thread.sleep(5000);
 
         System.out.println("异步方法执行,当前线程名称：" + Thread.currentThread().getName());
 
         List<Person> logic = businessServiceDemo.logic();
 
         return CompletableFuture.completedFuture(logic);
+    }
+
+    @Async("taskExecutor1")
+    public void normalPool() {
+        System.out.println("异步方法执行,当前线程名称：" + Thread.currentThread().getName());
+        System.out.println("普通的异步方法");
+    }
+
+    /**
+     * 普通的线程池
+     * @param runnable
+     */
+    public void testThreadPoolExecutor(Runnable runnable) throws InterruptedException {
+//        Thread.sleep(10000);
+        executor.execute(runnable);
     }
 
     @Bean(name = "taskExecutor1")
--- jdk8_demo/src/main/java/com/example/jdk8_demo/controller/TestController1.java
+++ jdk8_demo/src/main/java/com/example/jdk8_demo/controller/TestController1.java
@@ -1,61 +1,62 @@
-package com.example.jdk8_demo;
+package com.example.jdk8_demo.controller;
 
 import com.example.jdk8_demo.async.AsyncDemo;
 import com.example.jdk8_demo.async.Person;
-import com.google.common.annotations.VisibleForTesting;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-@SpringBootTest
-@VisibleForTesting
-class Jdk8DemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+@RestController
+public class TestController1 {
 
     @Autowired
     private AsyncDemo asyncDemo;
 
-    @Test
+    @GetMapping(value = "async")
     public void testAsync() throws InterruptedException, ExecutionException {
 
         System.out.println("当前线程开始执行");
-        // 第一个任务
+        // 第一个任务，有返回结果
         CompletableFuture<List<String>> listCompletableFuture = executeWith1();
-        // 第二个任务
+        // 第二个任务，有返回结果
         CompletableFuture<List<Person>> listCompletableFuture1 = executeWith2();
         CompletableFuture<List<Person>> listCompletableFuture2 = executeWith2();
         CompletableFuture<List<Person>> listCompletableFuture3 = executeWith2();
 
+        // 普通异步执行，没有返回结果
+        asyncDemo.normalPool();
+        // 等待三个任务全部结束
         CompletableFuture.allOf(listCompletableFuture, listCompletableFuture1, listCompletableFuture2, listCompletableFuture3).join();
-
+        // 打印一下三个任务是否全部结束
         System.out.println(listCompletableFuture1.isDone());
         System.out.println(listCompletableFuture2.isDone());
         System.out.println(listCompletableFuture3.isDone());
 
-        Thread.sleep(10000);
-        System.out.println("测试方法执行结束");
-    }
+        // 简单线程池实现
+        asyncDemo.testThreadPoolExecutor(() -> {
+            System.out.println("线程池的使用");
+        });
 
+        System.out.println("测试方法执行结束");
+
+    }
 
     public CompletableFuture<List<String>> executeWith1() throws InterruptedException, ExecutionException {
         CompletableFuture<List<String>> listCompletableFuture = asyncDemo.asyncMethodDemo1();
 
-        List<String> strings = listCompletableFuture.get();
+//        List<String> strings = listCompletableFuture.get();
 
-        System.out.println(strings);
+//        System.out.println(strings);
         return listCompletableFuture;
     }
 
     public CompletableFuture<List<Person>> executeWith2() throws InterruptedException, ExecutionException {
         CompletableFuture<List<Person>> listCompletableFuture = asyncDemo.asyncMethodDemo2();
-        System.out.println(listCompletableFuture.get());
+//        System.out.println(listCompletableFuture.get());
         return listCompletableFuture;
     }
 
